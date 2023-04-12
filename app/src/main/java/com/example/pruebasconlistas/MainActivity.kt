fb6553da6f5155665950b835ef2aac95f512e5cf
metalcry1 <metalcry1@gmail.com>
2023-04-12 18:54:59 +0200

    Pruebas con otro tipo de coleccion tipo Set:

--- app/src/main/java/com/example/pruebasconlistas/MainActivity.kt
+++ app/src/main/java/com/example/pruebasconlistas/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
 
 
         }else{
-            println("contiene: ${daysOfWeek.toString()}")
+            println("Elementos en daysOfWeek: ${daysOfWeek.toString()}")
         }
 
         println(daysOfWeek.filter {it.contains('a')})
@@ -41,6 +41,24 @@ class MainActivity : AppCompatActivity() {
 
         }
         println("Elementos en daysOfWeek2: ${daysOfWeek2.toString()}")
+
+
+        val daysOfWeek3 = daysOfWeek.toSet()
+
+        daysOfWeek.add(1,"Daniel")
+
+        println("Elementos en daysOfWeek3: ${daysOfWeek3.toString()}")
+
+        val numbersOfWeek : MutableList<Int> = mutableListOf(7,6,5,4,3,2,1,0)
+
+        println("Elementos en numbersOfWeek: ${numbersOfWeek.toString()}")
+
+
+        numbersOfWeek.add(8,8)
+
+        println("Elementos en numbersOfWeek: ${numbersOfWeek.toString()}")
+        println("Elementos ordenados en numbersOfWeek: ${numbersOfWeek.toSortedSet()}")
+
     }
 
     fun convertToSet(){
